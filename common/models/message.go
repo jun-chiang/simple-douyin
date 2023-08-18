f943f7d529ff246fc79828f0d7f29fda28b1ce9a
deltaLRD <ruidongli2002@gmail.com>
2023-08-18 16:25:44 +0800

    update

--- common/models/message.go
+++ common/models/message.go
@@ -1,18 +1,14 @@
 package models
 
 import (
-	"time"
-
 	"gorm.io/gorm"
 )
 
 type Message struct {
 	gorm.Model
-	ID         uint      `gorm:"primaryKey;autoIncrement" json:"id"`
-	FromUserID uint      `gorm:"column:from_user_id" json:"from_user_id"`
-	ToUserID   uint      `gorm:"column:to_user_id" json:"to_user_id"`
-	Content    string    `gorm:"column:content" json:"content"`
-	CreatedAt  time.Time `json:"create_time"`
-	UpdatedAt  time.Time
-	DeletedAt  gorm.DeletedAt
+	ID         uint   `gorm:"primaryKey;autoIncrement" json:"id"`
+	FromUserID uint   `gorm:"column:from_user_id" json:"from_user_id"`
+	ToUserID   uint   `gorm:"column:to_user_id" json:"to_user_id"`
+	Content    string `gorm:"column:content" json:"content"`
+	CreateTime uint   `json:"create_time"`
 }
--- messageservice/main.go
+++ messageservice/main.go
@@ -1,14 +1,28 @@
 package main
 
 import (
-	api "github.com/jun-chiang/simple-douyin/messageservice/kitex_gen/api/messageservice"
 	"log"
+
+	"github.com/cloudwego/kitex/pkg/rpcinfo"
+	"github.com/cloudwego/kitex/server"
+	api "github.com/jun-chiang/simple-douyin/messageservice/kitex_gen/api/messageservice"
+	"github.com/kitex-contrib/registry-nacos/registry"
 )
 
 func main() {
-	svr := api.NewServer(new(MessageServiceImpl))
 
-	err := svr.Run()
+	r, err := registry.NewDefaultNacosRegistry()
+	if err != nil {
+		panic(err)
+	}
+
+	svr := api.NewServer(
+		new(MessageServiceImpl),
+		server.WithServerBasicInfo(&rpcinfo.EndpointBasicInfo{ServiceName: "messageservice"}),
+		server.WithRegistry(r),
+	)
+
+	err = svr.Run()
 
 	if err != nil {
 		log.Println(err.Error())
