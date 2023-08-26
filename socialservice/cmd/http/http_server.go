dd6ae4a267e861cce964a3b324b9ec864f37f703
蒋天俊 <jiang_tian_jun@163.com>
2023-08-26 21:49:24 +0800

    feat(socialService): 为socialService服务配置服务注册

--- socialservice/cmd/http/http_server.go
+++ socialservice/cmd/http/http_server.go
@@ -1,14 +1,30 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/cloudwego/hertz/pkg/app/server"
+	"github.com/cloudwego/hertz/pkg/app/server/registry"
+	"github.com/cloudwego/hertz/pkg/common/utils"
+	"github.com/hertz-contrib/registry/nacos"
+	"github.com/jun-chiang/simple-douyin/common/consts"
+	"github.com/jun-chiang/simple-douyin/common/models"
 	"github.com/jun-chiang/simple-douyin/socialservice/api/routers"
 )
 
 func main() {
 	// 执行数据迁移，创建数据表
 	// migrations.Migration()
-	srv := server.Default(server.WithHostPorts("192.168.209.159:8091"))
+	srvAddr := fmt.Sprintf("%s:%d", consts.ProjectDevelopIP, 8091)
+	srv := server.Default(server.WithHostPorts(srvAddr),
+		server.WithRegistry(
+			nacos.NewNacosRegistry(models.GetNacosNamingClient().Client),
+			&registry.Info{
+				ServiceName: "social-service",
+				Addr:        utils.NewNetAddr("tcp", srvAddr),
+				Weight:      10,
+			}),
+	)
 
 	routers.InitRouter(srv.Engine)
 
